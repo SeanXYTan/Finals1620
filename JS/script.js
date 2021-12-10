9b1b246e22f3333c597129bef8658d11a917e59d
Sean <Waffuruki@hotmail.com>
2021-12-10 12:52:39 -0800

    Updated script.js
    - Added the function "submit_form" that prints the form to the console.
    - added an event listener to the the submit button.

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,30 @@
-toggle = document.querySelector(".toggle")
+const toggle = document.querySelector(".toggle");
+const submitbtn = document.querySelector("#frm-btn");
 
-toggle.addEventListener("click", swap_themes)
+toggle.addEventListener("click", swap_themes);
 
-function swap_themes(event) {
-    listelement = document.getElementsByTagName('*')
+function swap_themes() {
+    listelement = document.getElementsByTagName('*');
     for (let i = 0; i < listelement.length; i++) {
         classesList = listelement[i].classList
         if (!classesList.contains('light-theme')) {
             classesList.add('light-theme')
             classesList.remove('dark-theme')
-        } 
+        }
         else {
             classesList.remove('light-theme')
             classesList.add('dark-theme')
-        }
-    }
+        };
+    };
+};
 
-}
+submitbtn.addEventListener('click', submit_form);
+
+function submit_form () {
+    let new_object = {
+        email: document.querySelector('#email').value,
+        name: document.querySelector('#name').value,
+        message: document.querySelector('#msg').value
+    };
+    console.log(new_object);
+};
